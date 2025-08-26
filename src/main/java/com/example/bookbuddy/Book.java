6b82442dcd6e9c80441c8626d3d26e010984da85
umarfchy <umarfchy@gmail.com>
2025-08-26 21:44:53 +0600

    feat: add book class

---
+++ src/main/java/com/example/bookbuddy/Book.java
@@ -0,0 +1,17 @@
+public class Book{
+    private int id;
+    private String name;
+
+    public Book(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getName(){
+        return name;
+    }
+}
