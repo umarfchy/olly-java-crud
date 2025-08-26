af909cd28ee28f58ed0f1a74dfb9d10ba2ea782d
umarfchy <umarfchy@gmail.com>
2025-08-26 23:54:35 +0600

    feat: add db conn class

---
+++ src/main/java/com/example/bookbuddy/DBConnection.java
@@ -0,0 +1,25 @@
+package com.example.bookbuddy;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    public static Connection getConnection() throws IOException, SQLException {
+        Properties props = new Properties();
+
+        try (FileInputStream fis = new FileInputStream("application.properties")) {
+            props.load(fis);
+        }
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+
+}
