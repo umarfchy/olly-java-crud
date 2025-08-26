567cc061abae7ec5aa0e12f2710febb01fae9103
umarfchy <umarfchy@gmail.com>
2025-08-26 21:17:36 +0600

    feat: print string

--- main/java/com/example/bookbuddy/App.java
+++ main/java/com/example/bookbuddy/App.java
@@ -1,7 +1,7 @@
 package com.example.bookbuddy;
 
 public class App {
-    public static void main( String[] args){
-        new App().run();
+    public static void main(String[] args){
+        System.out.println("Hey Buddy");
     }
 }
