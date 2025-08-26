0b58564dd82875252e5af3a3673e523126491643
umarfchy <umarfchy@gmail.com>
2025-08-26 23:54:35 +0600

    feat: add db conn class

--- src/main/java/com/example/bookbuddy/App.java
+++ src/main/java/com/example/bookbuddy/App.java
@@ -1,18 +1,13 @@
 package com.example.bookbuddy;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    String url = "jdbc:postgresql://localhost:5432/mydb";
-    String user = "admin";
-    String password = "admin123";
 
-    try (Connection conn = DriverManager.getConnection(url, user, password)) {
+    try (Connection conn = DBConnection.getConnection()) {
       System.out.println(conn.getMetaData().toString());
       System.out.println("Connection successful;");
     } catch (Exception e) {
