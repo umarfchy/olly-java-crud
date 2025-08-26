91b8e0e3b7e9c9f7c6d6a8c18bd791d2c2fdaa07
umarfchy <umarfchy@gmail.com>
2025-08-26 21:51:57 +0600

    feat: add books and logs in console

--- src/main/java/com/example/bookbuddy/App.java
+++ src/main/java/com/example/bookbuddy/App.java
@@ -1,7 +1,26 @@
 package com.example.bookbuddy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hey Buddy");
+      List<Book>  books = new ArrayList<>();
+
+      Book book1 = new Book(1, "book-1");
+      Book book2 = new Book(2, "book-2");
+      Book book3 = new Book(3, "book-3");
+
+      books.add(book1);
+      books.add(book2);
+      books.add(book3);
+
+      System.out.println("These are the available books");
+      for (Book book: books){
+        System.out.println("ID: " + book.getId() + " Name: " + book.getName());
+      }
+    
     }
+
+
 }
